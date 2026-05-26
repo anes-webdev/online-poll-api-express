7f71e5649866b1d7cebe7507e8fb079f89c8ed11
anes-webdev <anes.webdev@gmail.com>
2026-05-26 11:49:37 +0330

    feat: add poll schema

--- src/routes/pollRoutes.ts
+++ src/routes/pollRoutes.ts
@@ -1,6 +1,14 @@
 import express from "express";
-import { createPoll, deletePoll, editPoll, getPoll, getPolls } from "../controllers/pollController.js";
+import {
+  createPoll,
+  deletePoll,
+  editPoll,
+  getPoll,
+  getPolls,
+} from "../controllers/pollController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { createPollSchema, editPollSchema } from "../validators/pollSchema.js";
 
 const router = express.Router();
 
@@ -8,7 +16,7 @@ router.use(authMiddleware);
 router.get("/", getPolls);
 router.get("/:id", getPoll);
 router.delete("/:id", deletePoll);
-router.put("/:id", editPoll);
-router.post("/", createPoll);
+router.put("/:id", validateRequest(editPollSchema), editPoll);
+router.post("/", validateRequest(createPollSchema), createPoll);
 
 export default router;
