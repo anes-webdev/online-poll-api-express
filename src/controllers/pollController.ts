ab37609a04bed9cb291b1e87b41ce516bd51db7e
anes-webdev <anes.webdev@gmail.com>
2026-06-07 17:57:49 +0330

    chore: add poll and vote limits for demo user

--- src/controllers/pollController.ts
+++ src/controllers/pollController.ts
@@ -3,6 +3,7 @@ import { prisma } from "../config/db.js";
 import { CreatePollBody, pollVotesQueryArgs } from "../types/poll.types.js";
 import { generateId } from "../utils/generateId.js";
 import { formatPollVotesResponse } from "../mappers/pollVotes.mapper.js";
+import { DEMO_USER_POLL_LIMIT } from "../constants/limits.js";
 
 const getPolls = async (req: Request, res: Response) => {
   // Todo: add search, filter:
@@ -32,12 +33,20 @@ const getPoll = async (req: Request, res: Response) => {
   return res.status(200).json(poll);
 };
 
-// Todo: add 10 limit for poll creation:
 const createPoll = async (
   req: Request<{}, {}, CreatePollBody>,
   res: Response,
 ) => {
   const { title, description, options } = req.body;
+
+  // Todo: update username to role:
+  // Check demo user limits:
+  if (req.user.username === "demo") {
+    const polls = await prisma.poll.findMany({ where: { createdBy: req.user.id } });
+    if (polls.length === DEMO_USER_POLL_LIMIT) {
+      return res.status(403).json({ message: `Demo user can create up to ${DEMO_USER_POLL_LIMIT} polls.` })
+    }
+  }
 
   try {
     await prisma.$transaction(async (prisma) => {
