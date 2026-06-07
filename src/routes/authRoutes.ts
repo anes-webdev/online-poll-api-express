f0f0ac42aac0e5a61088268240b8cf2a5993215b
anes-webdev <anes.webdev@gmail.com>
2026-06-07 12:05:50 +0330

    feat: add demo signin

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { signin, logout } from "../controllers/authController.js";
+import { signin, logout, demoSignin } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/demo-signin", demoSignin);
 router.post("/logout", logout);
 
 export default router;
